030652bb5c36cb79f5a6ee074d2e7f5ddf29fb1f
Tim Bright <timbright@MacBook-Pro-Tim.local>
2019-12-09 11:25:48 +0600

    created functions: render, createElement

---
+++ src/utils/utils.js
@@ -0,0 +1,31 @@
+const RenderPosition = {// указывает место вставки контента
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+
+const render = (container, element, place) => { // вставляет элемент в указанное место
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+
+export {
+  RenderPosition,
+  render,
+  createElement
+
+};
